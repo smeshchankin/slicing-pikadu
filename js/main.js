6750c7826d8df3edde78a9cd85711ac2e30f4b3f
Sergey Meshchankin <smeshchankin@gmail.com>
2020-11-09 23:21:25 +0200

    [PD-003][FIX] Adaptive: menu preventDefault

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     let menu = document.querySelector('.sidebar');
 
     menuToggle.addEventListener('click', function(event) {
+        event.preventDefault();
         menu.classList.toggle('visible');
     });
 }());
