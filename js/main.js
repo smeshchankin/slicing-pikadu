b4ad835021c457150918f1072e7eedeb834e5c62
Sergey Meshchankin <smeshchankin@gmail.com>
2020-11-16 22:52:53 +0200

    [PD-003] Utils: add applySelector()

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 (function() {
-    const elems = {
+    const elems = applySelector({
         menu: {
-            layout: document.querySelector('.sidebar'),
-            button: document.querySelector('#menuToggle')
+            layout: '.sidebar',
+            button: '#menuToggle'
         }
-    };
+    });
+    console.log(elems);
 
     elems.menu.button.addEventListener('click', function(event) {
         event.preventDefault();
         elems.menu.layout.classList.toggle('visible');
     });
+
+    function applySelector(obj) {
+        const res = {};
+        Object.keys(obj).forEach(key => {
+            const val = obj[key];
+            res[key] = typeof val === 'object' && val !== null
+                ? applySelector(val) : document.querySelector(val);
+        });
+        return res;
+    }
 }());
